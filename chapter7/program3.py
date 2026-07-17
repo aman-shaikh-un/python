52f1bdb7ff855ec3b77f4d6b8a48e1e3811ab57a
Aman Shaikh <aman.shaikh.un3481@gmail.com>
2026-07-17 19:29:36 +0000

    Add files via upload

---
+++ chapter7/program3.py
@@ -0,0 +1,5 @@
+list = ["Aman", 24 ,24.90 ,"shoeb" , True ]
+i=0
+while(i < len(list)):
+    print(list[i])
+    i += 1
---
+++ chapter7/quiz.py
@@ -0,0 +1,5 @@
+#print "bytesbyaman" 100 times using while loop
+i = 1
+while( i < 100):
+    i+=1
+    print("bytesbyaman")
